c648f214dd3f17fb36d13233338880d430b1c5ff
ggpwnkthx <ibjessup@gmail.com>
2021-03-18 17:26:05 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,11 @@ docker plugin disable $SCOPE_USER/$PLUGIN_NAME
 docker plugin rm $SCOPE_USER/$PLUGIN_NAME
 
 # Build
-docker build -t $SCOPE_USER/$PLUGIN_NAME .
-CONTAINER_ID=$(docker create $SCOPE_USER/$PLUGIN_NAME true)
-mkdir -p plugin/rootfs
-docker export "$CONTAINER_ID" | tar -x -C plugin/rootfs
-docker rm -vf "$CONTAINER_ID"
-docker rmi $SCOPE_USER/$PLUGIN_NAME
-docker plugin create $SCOPE_USER/$PLUGIN_NAME ./plugin
-
-# Enable
+docker build -t $SCOPE_USER/$PLUGIN_NAME . && \
+CONTAINER_ID=$(docker create $SCOPE_USER/$PLUGIN_NAME true) && \
+mkdir -p plugin/rootfs && \
+docker export "$CONTAINER_ID" | tar -x -C plugin/rootfs && \
+docker rm -vf "$CONTAINER_ID" && \
+docker rmi $SCOPE_USER/$PLUGIN_NAME && \
+docker plugin create $SCOPE_USER/$PLUGIN_NAME ./plugin && \
 docker plugin enable $SCOPE_USER/$PLUGIN_NAME
