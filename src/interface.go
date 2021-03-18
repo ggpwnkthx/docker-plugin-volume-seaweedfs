23302434841d755af15d643f87c6f01d6c5f293f
ggpwnkthx <ibjessup@gmail.com>
2021-03-18 15:28:11 -0400

    functions make more sense

--- src/interface.go
+++ src/interface.go
@@ -25,49 +25,49 @@ func (d *volumeDriver) listVolumes() []*volume.Volume {
 }
 
 func (d *volumeDriver) mountVolume(v *dockerVolume) error {
+	var args []string
+	args = append(args, "mount")
+	args = append(args, "-dir="+v.Mountpoint)
+	args = append(args, "-dirAutoCreate")
+	args = append(args, "-volumeServerAccess=filerProxy")
+	for _, option := range v.Options {
+		args = append(args, option)
+	}
+	cmd := exec.Command("/usr/bin/weed", args...)
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	v.PID = cmd.Process.Pid
 	return nil
 }
 
 func (d *volumeDriver) removeVolume(v *dockerVolume) error {
+	err := d.unmountVolume(v)
+	if err != nil {
+		return err
+	}
+	delete(d.volumes, v.Name)
+	return nil
+}
+
+func (d *volumeDriver) unmountVolume(v *dockerVolume) error {
 	if v.Connections == 0 {
 		cmd, _ := os.FindProcess(v.PID)
 		err := cmd.Kill()
 		if err != nil {
 			return err
 		}
-		delete(d.volumes, v.Name)
 	}
 	return nil
 }
 
-func (d *volumeDriver) unmountVolume(v *dockerVolume) error {
-	return nil
-}
-
 func (d *volumeDriver) updateVolume(v *dockerVolume) error {
-	if _, found := d.volumes[v.Name]; found {
-		d.volumes[v.Name] = v
-	} else {
-		if _, err := os.Stat(v.Mountpoint); err != nil {
-			if os.IsNotExist(err) {
-				os.Mkdir(v.Mountpoint, 0755)
-			}
+	d.volumes[v.Name] = v
+	if _, err := os.Stat(v.Mountpoint); err != nil {
+		if os.IsNotExist(err) {
+			os.MkdirAll(v.Mountpoint, 760)
 		}
-		var args []string
-		args = append(args, "mount")
-		args = append(args, "-dir="+v.Mountpoint)
-		args = append(args, "-dirAutoCreate")
-		args = append(args, "-volumeServerAccess=filerProxy")
-		for _, option := range v.Options {
-			args = append(args, option)
-		}
-		cmd := exec.Command("/usr/bin/weed", args...)
-		err := cmd.Run()
-		if err != nil {
-			return err
-		}
-		v.PID = cmd.Process.Pid
-		d.volumes[v.Name] = v
 	}
 	return nil
 }
