aa8af177b29852f569f5ef54d12ea77bb9789d2a
ggpwnkthx <ibjessup@gmail.com>
2021-03-19 17:07:14 -0400

    mount manager
    make it actually run more than once

--- src/interface.go
+++ src/interface.go
@@ -94,14 +94,16 @@ func (d *volumeDriver) updateVolume(v *dockerVolume) error {
 }
 
 func (d *volumeDriver) manager() {
-	for _, v := range d.volumes {
-		if v.CMD.ProcessState.Exited() {
-			if v.Tries < 3 {
-				v.sync.Unlock()
-				v.CMD.Start()
-				v.sync.Lock()
-			} else {
-				d.removeVolume(v)
+	for {
+		for _, v := range d.volumes {
+			if v.CMD.ProcessState.Exited() {
+				if v.Tries < 3 {
+					v.sync.Unlock()
+					v.CMD.Start()
+					v.sync.Lock()
+				} else {
+					d.removeVolume(v)
+				}
 			}
 		}
 	}
