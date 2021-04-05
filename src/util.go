2916f0abed9a00d87249fa89346f55b6f29d1647
Isaac B. Jessup <ibjessup@gmail.com>
2021-04-05 10:39:21 -0400

    Update util.go

--- src/util.go
+++ src/util.go
@@ -80,9 +80,11 @@ func WaitForStdLine(needle string, haystack *os.File, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for {
 		line, _ := bufio.NewReader(haystack).ReadString('\n')
-		logerr("scanning: " + line)
-		if strings.Contains(line, needle) {
-			break
+		if len(line) > 0 {
+			logerr("scanning: " + line)
+			if strings.Contains(line, needle) {
+				break
+			}
 		}
 	}
 }
