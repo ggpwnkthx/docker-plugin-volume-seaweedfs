fc81d39c9885cfed72d6b70f64362438d882d3d3
ggpwnkthx <ibjessup@gmail.com>
2021-04-22 13:59:30 -0400

    grpc proxy tests

--- proxy/entrypoint.sh
+++ proxy/entrypoint.sh
@@ -3,17 +3,17 @@ if [ -d /var/lib/docker/plugins/seaweedfs/$1 ]; then
     cat <<EOF >> /etc/haproxy/haproxy.cfg
 backend http
     mode http
-    server http filer:8888
+    server http filer:8888 check
 backend grpc
     mode http
-    server grpc filer:18888 ssl verify none alpn h2
+    server grpc filer:18888 check proto h2
 frontend http
     mode http
     bind unix@/var/lib/docker/plugins/seaweedfs/$1/http.sock
     default_backend http
 frontend grpc
     mode http
-    bind unix@/var/lib/docker/plugins/seaweedfs/$1/grpc.sock alpn h2
+    bind unix@/var/lib/docker/plugins/seaweedfs/$1/grpc.sock proto h2
     default_backend grpc
 EOF
     /usr/local/sbin/haproxy -f /etc/haproxy/haproxy.cfg
